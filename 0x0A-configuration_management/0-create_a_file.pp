aeca6c8b9ce8af2205e569f7f98c4980b64c7e6d
blackoaz <pauloakello56@gmail.com>
2023-06-27 08:33:55 -0700

    updates to task 1, adding comment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,11 @@
+#creating a file with puppet
 $file_path = '/tmp/school'
-file {$file_path:
+
+file {'creating a file':
 ensure => file,
 mode => '0744',
 owner => 'www-data',
 group => 'www-data',
 content => 'I love Puppet',
+path => $file_path
 }
