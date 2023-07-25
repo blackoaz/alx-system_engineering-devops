710533b23a9d3ef794a0aee0e41fd9321557a199
blackoaz <pauloakello56@gmail.com>
2023-07-25 13:28:22 -0700

    updating task 2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,15 +11,14 @@ import sys
 url = "https://jsonplaceholder.typicode.com"
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             id = int(sys.argv[1])
             user_res = requests.get('{}/users/{}'.format(url, id)).json()
             todos_res = requests.get('{}/todos'.format(url)).json()
-            user_name = user_res.get('name')
+            user_name = user_res.get('username')
             todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            todos_done = list(filter(lambda x: x.get('completed'), todos))
             with open('{}.csv'.format(id), 'w') as file:
                 for todo in todos:
                     file.write(
