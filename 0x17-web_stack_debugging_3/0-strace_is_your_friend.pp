341b8a4356e7a57c051e7883cdedeae1a5f2e3e0
blackoaz <pauloakello56@gmail.com>
2023-08-08 22:12:40 -0700

    adding updates

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,9 @@
-# Fix 500 error when a GET HTTP method is requested to Apache web server
-
-exec {'replace':
-  provider => shell,
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+# Web Stack debugging fix typo in config file
+exec { 'fix-wordpress':
+  environment => ['DIR=/var/www/html/wp-settings.php',
+                  'OLD=phpp',
+                  'NEW=php'],
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0, 1]
 }
